9211bc992d0c183f15295db867a4ce5d4d01ee7d
tim-xia <654027759@126.com>
2018-04-01 16:21:12 +0800

    first commit

---
+++ MVCDemo/Controllers/MVCDemoController.cs
@@ -0,0 +1,41 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class MVCDemoController : Controller
+    {
+        // GET: MVCDemo
+        public ActionResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 提交
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            //TODO: 发送邮件给晚会组织者
+            return View("Thanks", response);
+        }
+
+
+        public ViewResult Thanks()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    /// <summary>
+    /// 宾客类 
+    /// </summary>
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+
+    }
+}
